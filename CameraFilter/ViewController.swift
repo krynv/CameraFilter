08ee272fd5bc5b5f45c34d4c7f01631010893f36
VK <DEFCONone1@users.noreply.github.com>
2017-10-30 09:40:30 +0000

    Initial commit

--- CameraFilter/ViewController.swift
+++ CameraFilter/ViewController.swift
@@ -7,19 +7,108 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var captureSession = AVCaptureSession()
+    var backCamera: AVCaptureDevice?
+    var frontCamera: AVCaptureDevice?
+    var currentCamera: AVCaptureDevice?
+    
+    var photoOutput: AVCapturePhotoOutput?
+    
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupCorrectFramerate(currentCamera: currentCamera!) // will default to 30fps unless stated otherwise
+        setupPreviewLayer()
+        startRunningCaptureSession()
     }
-
+    
+    func setupCaptureSession() {
+        // should support anything up to 1920x1080 res, incl. 240fps @ 720p
+        captureSession.sessionPreset = AVCaptureSession.Preset.high
+    }
+    
+    func setupDevice() {
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        let devices = deviceDiscoverySession.devices
+        
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back {
+                backCamera = device
+            }
+            else if device.position == AVCaptureDevice.Position.front {
+                frontCamera = device
+            }
+        }
+        
+        currentCamera = backCamera
+    }
+    
+    func setupInputOutput() {
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            captureSession.addInput(captureDeviceInput)
+            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func setupCorrectFramerate(currentCamera: AVCaptureDevice) {
+        for vFormat in currentCamera.formats {
+            //see available types
+            //print("\(vFormat) \n")
+    
+            var ranges = vFormat.videoSupportedFrameRateRanges as [AVFrameRateRange]
+            let frameRates = ranges[0]
+            
+            do {
+                //set to 240fps - available types are: 30, 60, 120 and 240 and custom
+                // lower framerates cause major stuttering
+                if frameRates.maxFrameRate == 240 {
+                    try currentCamera.lockForConfiguration()
+                    currentCamera.activeFormat = vFormat as AVCaptureDevice.Format
+                    //for custom framerate set min max activeVideoFrameDuration to whatever you like, e.g. 1 and 180
+                    currentCamera.activeVideoMinFrameDuration = frameRates.minFrameDuration
+                    currentCamera.activeVideoMaxFrameDuration = frameRates.maxFrameDuration
+                }
+            }
+            catch {
+                print("Could not set framerate")
+            }
+        }
+    }
+    
+    func setupPreviewLayer() {
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        cameraPreviewLayer?.frame = self.view.frame
+        
+        //set preview in background, allows for elements to be placed in the foreground
+        self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+        
+    }
+    
+    func startRunningCaptureSession() {
+        captureSession.startRunning()
+        backCamera?.unlockForConfiguration()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
